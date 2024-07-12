9e862a3e2b16be33209b5218c0c44a5507710516
Akhlaq Ahmed Shaikh <akhlaqshaikh03@gmail.com>
2024-07-12 15:26:45 +0500

    Another small fix

--- src/components/input/inputs.jsx
+++ src/components/input/inputs.jsx
@@ -25,44 +25,46 @@ const InputField = ({ setQuery, setUnits }) => {
     });
   };
   return (
-    <div className="flex flex-row justify-center my-6">
-      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-        <input
-          type="text"
-          placeholder="search city..."
-          value={city}
-          onChange={(e) => setCity(e.currentTarget.value)}
-          onKeyDown={handleKeyPress}
-          className="text-gray-500 text-xl font-light p-2 ml-16 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
-        />
+    <>
+      <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+          <input
+            type="text"
+            placeholder="search city..."
+            value={city}
+            onChange={(e) => setCity(e.currentTarget.value)}
+            onKeyDown={handleKeyPress}
+            className="text-gray-500 text-xl font-light p-2 ml-16 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
+          />
 
-        <BiSearch
-          size={30}
-          className="cursor-pointer transition ease-out hover:scale-125"
-          onClick={handleSearch}
-        />
-        <BiCurrentLocation
-          size={30}
-          className="cursor-pointer transition ease-out hover:scale-125"
-          onClick={handleLocationSearch}
-        />
+          <BiSearch
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleSearch}
+          />
+          <BiCurrentLocation
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleLocationSearch}
+          />
+        </div>
+        <div className="flex flex-row w-1/4 items-center justify-center">
+          <button
+            className="text-2xl font-medium transition ease-out hover:scale-150"
+            onClick={() => setUnits("metric")}
+          >
+            &deg;C
+          </button>
+          <p className="text-2xl font-medium mx-1"> |</p>
+          <button
+            className="text-2xl font-medium transition ease-out hover:scale-150"
+            onClick={() => setUnits("imperial")}
+          >
+            &deg;F
+          </button>
+        </div>
       </div>
-      <div className="flex flex-row w-1/4 items-center justify-center">
-        <button
-          className="text-2xl font-medium transition ease-out hover:scale-150"
-          onClick={() => setUnits("metric")}
-        >
-          &deg;C
-        </button>
-        <p className="text-2xl font-medium mx-1"> |</p>
-        <button
-          className="text-2xl font-medium transition ease-out hover:scale-150"
-          onClick={() => setUnits("imperial")}
-        >
-          &deg;F
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
